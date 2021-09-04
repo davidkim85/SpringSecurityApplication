ff0ca356d0ed248e1fba3dc7a6ec1fbbcdf34630
davidkim85 <poramok@gmail.com>
2021-09-04 14:35:09 +0300

    with form

--- src/main/java/com/davidos/appdemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/davidos/appdemo/security/ApplicationSecurityConfig.java
@@ -41,7 +41,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/login").permitAll();
     }
 
     @Override
